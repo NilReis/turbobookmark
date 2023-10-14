85786aa3171e45a5ef137df9e5aa1118ac254325
NilReis <nil.f.reis@hotmail.com>
2023-10-14 13:31:02 -0300

    adfaf

--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
-document.getElementById('showSidebar').addEventListener('click', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      if (tabs[0]) {
-          chrome.scripting.executeScript({
-              target: {tabId: tabs[0].id},
-              files: ['sidebar.js']
-          });
-      }
-  });
+document.getElementById('showSidebar').addEventListener('click', async function() {
+  let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  if (tab) {
+      chrome.scripting.executeScript({
+          target: {tabId: tab.id},
+          files: ['sidebar.js']
+      });
+  }
 });
